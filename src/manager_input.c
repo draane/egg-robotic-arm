484897ac4e832856adc6c6cf80cac227847845cc
Hook25 <massigi@live.it>
2018-05-08 15:12:15 +0200

    Separed manager_input.c and utils.h

--- src/manager_input.c
+++ src/manager_input.c
@@ -4,16 +4,9 @@
 #include<signal.h>
 #include<sys/types.h>
 #include"manager_input.h"
+#include"utils.h"
 
 #define MAX_PINS 8
-#define DEBUG 1
-
-#if DEBUG
-#include<stdio.h>
-#define PRINT(...) printf(__VA_ARGS__)
-#else
-#define PRINT(x) ;
-#endif
 
 pid_t PIN_PID[MAX_PINS];
 
@@ -26,24 +19,24 @@ void kill_all_sons(int limit){
 }
 
 void input_manager(void){
-  PRINT("Started all process, destruction of the earth now being done");
+  PRINT("Started all process, destruction of the earth now being done\n");
   kill_all_sons(MAX_PINS);
 }
 
 void start_input(void){
-  PRINT("Input reader started");
+  PRINT("Input reader started\n\n");
   for(int i = 0; i<MAX_PINS; i++){
-    PRINT("Starting process %i", i + 1);
+    PRINT("Starting process %i\n", i + 1);
     int l_pid = fork();
     if(l_pid == 0){  //Son process
      pause(); 
     }else if(l_pid == -1){
-      PRINT("Fatal error occurred in the creation of the %i process, aborting");
+      PRINT("Fatal error occurred in the creation of the %i process, aborting\n");
       kill_all_sons(i);      
       return;
     }else{
       PIN_PID[i] = l_pid;
-      PRINT("Process %i created successfully", l_pid);
+      PRINT("Process %i created successfully\n", l_pid);
     }
   }
   input_manager(); 
