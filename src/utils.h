fb7cd97396a5107bdce189c1dd27f30ff26ffbe5
Hook25 <massigi@live.it>
2018-05-09 13:54:12 +0200

    Update to avoid multiple definition on import utils.h

--- src/utils.h
+++ src/utils.h
@@ -1,10 +1,12 @@
-#ifndef DEBUG
-#define DEBUG 1
-#endif
-
-#if DEBUG
-#include<stdio.h>
-#define PRINT(...) printf(__VA_ARGS__)
-#else
-#define PRINT(x) ;
+#ifndef DEV_UTILS_H
+  #define DEV_UTILS_H
+  #ifndef DEBUG
+    #define DEBUG 1
+  #endif
+  #if DEBUG
+    #include<stdio.h>
+    #define PRINT(...) printf(__VA_ARGS__)
+  #else
+    #define PRINT(x) ;
+  #endif
 #endif
