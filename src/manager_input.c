25f69710d8bef85a13505e889a4d163636834324
Hook25 <massigi@live.it>
2018-05-11 16:48:43 +0200

    Working piping system

--- src/manager_input.c
+++ src/manager_input.c
@@ -11,51 +11,79 @@
 #define PRESENT 1
 #define EMPTY !PRESENT 
 
-typedef int status_t;
-
 #define MAX_PINS 8
 
-struct pin_status{
-  status_t pin_status;
-  sem_t ready;
-};
+typedef int pinstatus;
 
-pid_t PIN_PID[MAX_PINS];
+#define READ_PIPE 0
+#define WRITE_PIPE 1
+
+#define ON 1
+#define OFF !ON
+
+typedef struct pidpipe{
+  int  pipe[2];
+  pid_t pid;
+  int value;
+} pidpipe;
+
+pidpipe PIN_PIPE_PID[MAX_PINS];
 
 //Kills all sons, used when a fatal error occurres or just
 //when the process has to be terminated
 void kill_all_sons(int limit){
   for(int i = 0; i < limit; i++){
-    kill(PIN_PID[i],SIGKILL);
+    kill(PIN_PIPE_PID[i].pid,SIGKILL);
   }
 }
 
-void child_pin_reader(int i){
-  PRINT("I'm process %i, just started\n");
-  pause();
+int read_pin(int n){
+  return n; 
+}
+
+void child_pin_reader(int n,int p[2]){
+  for(;;){
+    close(p[READ_PIPE]);
+    int val = read_pin(n);
+    write(p[WRITE_PIPE],&val,sizeof(int));
+    sleep(5);
+  }
 }
 
 void input_manager(void){
+  for(int i = 0; i<MAX_PINS; i++){
+    int res = -1;
+    int bytes = read(PIN_PIPE_PID[i].pipe[READ_PIPE], &res, sizeof(int));
+    PRINT("Readed from i > %i\n", i);
+  }
   PRINT("Started all process, destruction of the earth now being done\n");
+  fflush(stdin);
   kill_all_sons(MAX_PINS);
+}
+
+void create_process(int i){
+  if(i == MAX_PINS){
+    return; 
+  }else{
+    PRINT("Starting %i\n", i);
+    pipe(PIN_PIPE_PID[i].pipe);
+    int pid = fork();
+    if(pid < 0){
+      PRINT("Fatal error occurred in the creation of a childi\n");
+      exit(1);
+    }else if(pid == 0){ // child;
+      child_pin_reader(i,PIN_PIPE_PID[i].pipe);
+      return;
+    }else{
+      close(PIN_PIPE_PID[i].pipe[WRITE_PIPE]);
+      PIN_PIPE_PID[i].pid = pid;
+      create_process(i+1);
+    }
+  }
 }
 
 void start_input(void){
   PRINT("Input reader started\n\n");
-  for(int i = 0; i<MAX_PINS; i++){
-    PRINT("Starting process %i\n", i + 1);
-    int loc_i = i;
-    int l_pid = fork();
-    if(l_pid == 0){  //Son process
-      child_pin_reader(loc_i); 
-    }else if(l_pid == -1){
-      PRINT("Fatal error occurred in the creation of the %i process, aborting\n");
-      kill_all_sons(i);      
-      return;
-    }else{
-      PIN_PID[i] = l_pid;
-      PRINT("Process %i created successfully\n", l_pid);
-    }
-  }
+  create_process(0);
   input_manager(); 
 }
