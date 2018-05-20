0158bd8217b1d5e4f578f7d4d1f343237fad6a3b
draane <andraane@libero.it>
2018-05-20 11:13:57 +0200

    add & fix end signal handlers. simplify pipe comunication

--- src/manager_output.c
+++ src/manager_output.c
@@ -19,8 +19,8 @@ int output_read_pipe, output_write_pipe;
 
 
 static void kill_all_sons(void);
-static int shutdown(void);
-static void output_manager_sigterm_handler (int signal);
+static int shutdown(const int exit_value);
+static void output_manager_end_signal_handler (int signal);
 
 static int get_data_from_manager (const int pipe, int* par1, int* par2, int* par3, int* par4);
 static int write_data_to_manager (const int pipe, const int par);
@@ -38,7 +38,7 @@ void output_manager(int pipe_write, int pipe_read, pid_t father_pid) {
 
   for ever {
     if (0 != get_data_from_manager(pipe_read, &eggs_in_the_case, &eggs_to_move, &eggs_to_order, &arduino_value) ) {
-      shutdown();
+      shutdown(1);
     }
 
     int i; // just a counter
@@ -73,13 +73,13 @@ void output_manager(int pipe_write, int pipe_read, pid_t father_pid) {
     }
 
     if (0 != write_data_to_manager(pipe_write, 0) ) {
-      shutdown();
+      shutdown(1);
     }
 
   }
 
-  sleep(8);
-  kill_all_sons();
+  //shutdown never get here
+  shutdown(1);
 
 }
 
@@ -93,16 +93,20 @@ void start_output(int pipe_write, int pipe_read, pid_t father_pid) {
 
   output_pin_pid = malloc(sizeof(pid_t)*OUTPUT_PIN_NUMBER);
 
+  int i;
+
+  for (i = 0; i<OUTPUT_PIN_NUMBER; i++)
+    output_pin_pid[i] = -1;
+
   output_read_pipe = pipe_read;
   output_write_pipe = pipe_write;
   output_father_pid = father_pid;
 
-  int i;
   for(i = 0; i<OUTPUT_PIN_NUMBER; i++) {
     pid_t pid = fork();
     if (pid == -1) {
       PRINT("Error at spawning child, closing\n");
-      shutdown();
+      shutdown(1);
     }
     else if(pid == 0) {
       output_pin_controller(output_pin[i]);
@@ -113,9 +117,10 @@ void start_output(int pipe_write, int pipe_read, pid_t father_pid) {
     }
   }
 
-
   //set handler for SIGTERM
-  signal(SIGTERM, output_manager_sigterm_handler);
+  signal(SIGTERM, output_manager_end_signal_handler);
+  //set handler for SIGINT, so it handles ^C
+  signal(SIGINT, output_manager_end_signal_handler);
 
   sleep(1); // just to make sure every process spawned
 
@@ -123,53 +128,28 @@ void start_output(int pipe_write, int pipe_read, pid_t father_pid) {
 }
 
 static int get_data_from_manager (const int pipe, int* par1, int* par2, int* par3, int* par4) {
-  char msg_received[MAX_INFO_TO_SEND_SIZE];
-  strcpy(msg_received, "\0");
-
-  read(pipe, msg_received, MAX_INFO_TO_SEND_SIZE);
-
-  if (strcmp(msg_received, "start\0") != 0) {
-    fprintf(stdout, "Output process didn't receive the start command as expected.\n");
-    return 1;
-  }
-  else {
-    write(pipe_output_write, "ack\0", MAX_INFO_TO_SEND_SIZE);
-  }
-
+  unsigned char msg_received[MAX_INFO_TO_SEND_SIZE];
   int parameters[NUM_PARAMETERS_RECEIVED];
-
   int i;
+
   for (i = 0; i< NUM_PARAMETERS_RECEIVED; i++) {
     read(pipe, msg_received, MAX_INFO_TO_SEND_SIZE);
     parameters[i] = atoi(msg_received);
-    fprintf(stdout, "received %s\n", msg_received);
-    write(pipe_output_write, "ack\0", MAX_INFO_TO_SEND_SIZE);
-  }
-
-  read(pipe, msg_received, MAX_INFO_TO_SEND_SIZE);
-  if (strcmp(msg_received, "finish_output\0") != 0) {
-    return 1;
-  }
-
-  fprintf(stdout, "received %s\n", msg_received);
-  write(pipe_output_write, "ack\0", MAX_INFO_TO_SEND_SIZE);
+    PRINT("received %s\n", msg_received);
+  };
 
   *par1 = parameters[0];
   *par2 = parameters[1];
   *par3 = parameters[2];
+  *par4 = parameters[2];
 
   return 0;
 }
 
 static int write_data_to_manager (const int pipe, const int par) {
-  char msg_received[MAX_INFO_TO_SEND_SIZE];
-  strcpy(msg_received, "\0");
-
-  write(pipe, "finish_output\0", MAX_INFO_TO_SEND_SIZE);
-  read(pipe_output_read, msg_received, MAX_INFO_TO_SEND_SIZE);
-  if (strcmp(msg_received, "ack\0") != 0) {
-    return 1;
-  }
+  char buffer[MAX_INFO_TO_SEND_SIZE];
+  sprintf(buffer, "%d", par);
+  write(pipe, buffer, MAX_INFO_TO_SEND_SIZE);
   return 0;
 }
 
@@ -181,7 +161,7 @@ void kill_all_sons(void) {
   PRINT("KILLING ALL OUTPUT_PIN PROCESSES...\n");
 
   int i;
-  for (i = 0; i < OUTPUT_PIN_NUMBER; i++) {
+  for (i = 0; i < OUTPUT_PIN_NUMBER && output_pin_pid[i] != -1; i++) {
     disable_pin(output_pin[i]);
     kill(output_pin_pid[i], SIGKILL);
   }
@@ -190,7 +170,7 @@ void kill_all_sons(void) {
   PRINT("\tDONE\n");
 }
 
-static int shutdown() {
+static int shutdown(const int exit_value) {
   //kill all child
   kill_all_sons();
 
@@ -201,9 +181,12 @@ static int shutdown() {
   //send END signal to father
   kill(SIGTERM, output_father_pid);
 
-  exit (1);
+  exit (exit_value);
 }
 
-static void output_manager_sigterm_handler (int signal) {
-  shutdown();
+static void output_manager_end_signal_handler (int signal) {
+  if (signal == SIGTERM)
+    shutdown(-1);
+  else
+    shutdown(-2);
 }
