d3ab006e7233a404f3ad413e52999bec726ded83
Andrea Dalla Costa <andrea.dallacosta@studenti.unitn.it>
2018-05-27 23:06:41 +0200

    realease 1.0

--- src/utils.h
+++ src/utils.h
@@ -9,29 +9,23 @@
   #else
     #define PRINT(x) ;
   #endif
-  // comment when arm is not installed.
-  // #define ARM_INSTALLED 1
 
-  // Hubi&Costa's stuff here.
   #define READ_PIPE 0
   #define WRITE_PIPE 1
   #define MAX_INFO_TO_SEND_SIZE 100
   #define DIM_OF_MSG_PIPE 2
   #define NUM_PARAMETERS_RECEIVED 3
 
-  // Max's stuff here
   #define START_MSG "1"
   #define OFFSET_OUTPUT_MSG 'a'
 
-
-  // Eggs and pins information
   #define NUMBER_OF_OUTPUT_BYTE 1
   #define NUMBER_OF_EGGS_IN_THE_BOX 6
 
-  // Raspberry
-  //#define ON_THE_RASPBERRY
+  // Comment when not compiling and executing on a Raspberry.
+  #define ON_THE_RASPBERRY
   #define NUM_PINS 8
 
-  // Robotic arm
-  //#define ARM_INSTALLED
+  // Comment when not using the robotic arm.
+  // #define ARM_INSTALLED
 #endif
