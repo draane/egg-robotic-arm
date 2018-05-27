7ef4e84e9d050ef85536ee367acd37d9ce49c7a3
Andrea Dalla Costa <andrea.dallacosta@studenti.unitn.it>
2018-05-27 17:38:33 +0000

    fix serial.c

--- src/serial.c
+++ src/serial.c
@@ -10,6 +10,7 @@
 #include <unistd.h>   // for usleep()
 #include <termios.h>  // POSIX terminal control definitions
 
+#include "utils.h"
 #include "arduino-serial-lib.h"
 
 #define ms_to_wait 100
@@ -17,13 +18,14 @@
 int serial_start (int port_number, int baud)
 {
   int fd = -1;
-  char base_port_string[13] = "/dev/ttyACM";
+  char base_port_string[13] = "/dev/ttyUSB";
 
   char port_string[15];
   if (port_number == -1) {
     int i;
     for (i = 0; i < 10 && fd == -1; i++) {
       sprintf(port_string, "%s%d", base_port_string, i);
+      PRINT("Try the serial: %s\n", port_string);
   		fd = serialport_init(port_string, baud);
     }
   }
@@ -34,22 +36,25 @@ int serial_start (int port_number, int baud)
 
   if (fd == -1)
     return -1;
-
   //send init message
-  uint8_t init_message = 7;
+/*  int init_message = 7;
+serialport_write(fd, "7\n");
   if (serialport_writebyte(fd, init_message) == -1)
   {
     serialport_close(fd);
 		return -2;
   }
-
+*/
   //read init response
   char eolchar = '\n';
   int timeout = 5000;
   char buf[4];
   if (serialport_read_until(fd, buf, eolchar, 4, timeout) < 0) {
+
+    if (serialport_read_until(fd, buf, eolchar, 4, timeout) < 0) {
     serialport_close(fd);
 		return -3;
+}
   }
 
   if ( buf[0] != '4' || buf[1] != '2' ) {
@@ -73,19 +78,18 @@ int send_message_to_arduino (int fd, int message) {
 	do {
     serialport_flush(fd);
     error = 0;
-
     if (serialport_writebyte(fd, message) == -1) {
       return -1;
     }
-
     if (serialport_writebyte(fd, message_id) == -1) {
       return -2;
     }
+PRINT("serial sent %d id: %d\n", message, message_id);
 
     if (serialport_read_until(fd, id_buffer, eolchar, 8, timeout) == -2) {
     	return -4;
     }
-
+PRINT("serial recived: %s\n", id_buffer);
     uint8_t id_return = atoi (id_buffer);
 
     if (id_return != message_id)
